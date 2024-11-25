757aebd4e6e3698ca4d3af32cac543cc468b8fd6
debbymariaa <135925863+debbymariaa@users.noreply.github.com>
2024-11-25 20:38:50 +0700

    to generic

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,24 +1,35 @@
-"use client";
+// button.tsx
 import React from 'react';
 
-type ButtonProps = {
-    variant: 'primary' | 'secondary' | 'error' | 'success';
-    children: React.ReactNode;
+export type ButtonVariant = 'primary' | 'secondary' | 'error' | 'success';
+
+interface ButtonProps<T extends ButtonVariant> {
+  variant: T;
+  children: React.ReactNode;
+  variantStyles: Record<T, string>;
+  onClick?: () => void;  // Optional onClick handler
+}
+
+const Button = <T extends ButtonVariant>({
+  variant,
+  children,
+  variantStyles,
+  onClick,
+}: ButtonProps<T>) => {
+  const buttonClass = variantStyles[variant];
+
+  return (
+    <button onClick={onClick} className={`py-2 px-4 rounded-full text-white ${buttonClass}`}>
+      {children}
+    </button>
+  );
 };
 
-const Button: React.FC<ButtonProps> = ({ variant, children }) => {
-    const variants = {
-        primary: 'bg-blue-500 hover:bg-blue-600 transition-colors duration-300 ease-in-out',
-        secondary: 'bg-gray-500 hover:bg-gray-600 transition-colors duration-300 ease-in-out',
-        error: 'bg-red-500 hover:bg-red-600 transition-colors duration-300 ease-in-out',
-        success: 'bg-green-500 hover:bg-green-600 transition-colors duration-300 ease-in-out'
-    }[variant];
-
-    return (
-        <button className={["py-2 px-4 rounded-full text-white", variants].join(" ")}>
-            {children}
-        </button>
-    );
+export const ButtonVariants: Record<ButtonVariant, string> = {
+  primary: 'bg-blue-500 hover:bg-blue-600 transition-colors duration-300 ease-in-out',
+  secondary: 'bg-gray-500 hover:bg-gray-600 transition-colors duration-300 ease-in-out',
+  error: 'bg-red-500 hover:bg-red-600 transition-colors duration-300 ease-in-out',
+  success: 'bg-green-500 hover:bg-green-600 transition-colors duration-300 ease-in-out',
 };
 
 export default Button;
